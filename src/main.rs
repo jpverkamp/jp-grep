df474495895173cd22cb8f6388bb73aec3dfb2cd
JP Verkamp <jverkamp@dropbox.com>
2024-08-28 15:47:01 -0400

    Rewrite with more structure
    * Implement a Regex enum with single characters, digits, and a simple sequence
    * Implement From<String> for Regex
    * Implement matches on Regex against any &[char]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,85 @@ use std::io::BufRead;
 
 use clap::Parser;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Regex {
+    Sequence(Vec<Regex>),
+    LiteralChar(char),
+    AnyDigit,
+}
+
+impl From<String> for Regex {
+    fn from(value: String) -> Self {
+        let mut sequence = vec![];
+        let mut chars = value.chars().peekable();
+
+        while let Some(c) = chars.next() {
+            let node = match c {
+                '\\' => {
+                    match chars.next() {
+                        Some('d') => Regex::AnyDigit,
+                        _ => unimplemented!()
+                    }
+                },
+                c => Regex::LiteralChar(c),
+            };
+
+            sequence.push(node);
+        }
+
+        Regex::Sequence(sequence)
+    }
+}
+
+impl Regex {
+    pub fn matches(&self, input: &[char]) -> bool {
+        // TODO: Do we care about remaining input at this level?
+        let (matched, _) = self.match_recur(input);
+        matched
+    }
+
+    fn match_recur<'a>(&self, input: &'a [char]) -> (bool, &'a [char]) {
+        // TODO: Fix this when we have potentially empty groups
+        if input.len() == 0 {
+            return (false, input);
+        }
+
+        match self {
+            // Single character matches
+            Regex::LiteralChar(c) => {
+                if input[0] == *c {
+                    return (true, &input[1..]);
+                }
+
+                return self.match_recur(&input[1..]);
+            },
+            Regex::AnyDigit => {
+                if input[0].is_digit(10) {
+                    return (true, &input[1..]);
+                }
+
+                return self.match_recur(&input[1..]);
+            },
+
+            // A sequence of matches, all of which must match
+            // If any fails, abort the entire sequence and advance to try again
+            Regex::Sequence(seq) => {
+                let mut remaining = input;
+                for node in seq {
+                    let (matched, new_remaining) = node.match_recur(remaining);
+                    if !matched {
+                        return self.match_recur(&input[1..]);
+                    }
+
+                    remaining = new_remaining;
+                }
+
+                return (true, remaining);
+            },
+        }
+    }
+}
+
 /// A custom grep implementation
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,31 +90,23 @@ struct Args {
     extended_regexp: String,
 }
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
-
 fn main() {
     let args = Args::parse();
+    let regex = Regex::from(args.extended_regexp);
 
     let stdin = std::io::stdin();
     let mut matches = 0;
 
     for line in stdin.lock().lines() {
         let input_line = line.unwrap();
-        if match_pattern(&input_line, &args.extended_regexp) {
+        let input_chars = input_line.chars().collect::<Vec<_>>();
+        
+        // TODO: Do we care about output? 
+        if regex.matches(&input_chars) {
             matches += 1;
             println!("{}", input_line);
         }
     }
 
-    if matches > 0 {
-        std::process::exit(0);
-    } else {
-        std::process::exit(1);
-    }
+    std::process::exit(if matches > 0 { 0 } else { 1 });
 }
