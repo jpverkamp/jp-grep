6ce03a82960efc84f652ccf4ddf8b29e6b2dc42e
JP Verkamp <jverkamp@dropbox.com>
2024-08-28 16:54:59 -0400

    Add support for negated groups [^abc]

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ enum Regex {
     Range(char, char),
     Sequence(Vec<Regex>),
     Choice(Vec<Regex>),
+    Not(Box<Regex>),
 }
 
 impl From<String> for Regex {
@@ -35,6 +36,14 @@ impl From<String> for Regex {
                 // TODO: Implement ranges
                 // TODO: Implement escaping in character groups
                 '[' => {
+                    // Handle negation
+                    let negated = if let Some('^') = chars.peek() {
+                        chars.next();
+                        true
+                    } else {
+                        false
+                    };
+
                     let mut choices = vec![];
                     while let Some(c) = chars.next() {
                         if c == ']' {
@@ -44,7 +53,11 @@ impl From<String> for Regex {
                         choices.push(Regex::Char(c));
                     }
 
-                    Regex::Choice(choices)
+                    if negated {
+                        Regex::Not(Box::new(Regex::Choice(choices)))
+                    } else {
+                        Regex::Choice(choices)
+                    }
                 },
 
                 // Single characters
@@ -66,10 +79,20 @@ impl Regex {
         matched
     }
 
+    fn allow_none(&self) -> bool {
+        // For each match type, determine if it can match an empty string
+        match self {
+            Regex::Char(_) => false,
+            Regex::Range(_, _) => false,
+            Regex::Sequence(seq) => seq.iter().all(|node| node.allow_none()),
+            Regex::Choice(seq) => seq.iter().any(|node| node.allow_none()),
+            Regex::Not(node) => node.allow_none(),
+        }
+    }
+
     fn match_recur<'a>(&self, input: &'a [char]) -> (bool, &'a [char]) {
-        // TODO: Fix this when we have potentially empty groups
         if input.len() == 0 {
-            return (false, input);
+            return (self.allow_none(), input);
         }
 
         match self {
@@ -84,6 +107,16 @@ impl Regex {
             Regex::Range(start, end) => {
                 if input[0] >= *start && input[0] <= *end {
                     return (true, &input[1..]);
+                }
+
+                return self.match_recur(&input[1..]);
+            },
+
+            // A negative match of any other matcher
+            Regex::Not(node) => {
+                let (matched, new_remaining) = node.match_recur(input);
+                if !matched {
+                    return (true, new_remaining);
                 }
 
                 return self.match_recur(&input[1..]);
@@ -155,59 +188,52 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_literal_char() {
-        let regex = Regex::from("a".to_string());
-        assert!(regex.matches("a"));
-        assert!(!regex.matches("b"));
-        assert!(regex.matches("ab"));
-        assert!(regex.matches("ba"));
+    macro_rules! test_regex {
+        ($name:ident, $regex:expr, $input:expr, $expected:expr) => {
+            #[test]
+            fn $name() {
+                let regex = Regex::from($regex.to_string());
+                assert_eq!(regex.matches($input), $expected);
+            }
+        };
     }
 
-    #[test]
-    fn test_any_digit() {
-        let regex = Regex::from(r"\d".to_string());
-        assert!(regex.matches("1"));
-        assert!(regex.matches("2"));
-        assert!(regex.matches("3"));
-        assert!(!regex.matches("a"));
-        assert!(!regex.matches("b"));
-        assert!(!regex.matches("c"));
-    }
+    test_regex!(single_char, "a", "a", true);
+    test_regex!(single_char_not, "a", "b", false);
+    test_regex!(single_char_first, "a", "abb", true);
+    test_regex!(single_char_middle, "a", "bab", true);
+    test_regex!(single_char_last, "a", "bba", true);
+    test_regex!(single_char_notmulti, "a", "bbb", false);
+    test_regex!(single_char_empty, "a", "", false);
 
-    #[test]
-    fn test_any_word() {
-        let regex = Regex::from(r"\w".to_string());
-        assert!(regex.matches("a"));
-        assert!(regex.matches("b"));
-        assert!(regex.matches("c"));
-        assert!(regex.matches("1"));
-        assert!(regex.matches("2"));
-        assert!(regex.matches("3"));
-        assert!(!regex.matches(" "));
-        assert!(!regex.matches("\n"));
-    }
+    test_regex!(any_digit, r"\d", "1", true);
+    test_regex!(any_digit_not, r"\d", "a", false);
+    test_regex!(any_digit_empty, r"\d", "", false);
+    test_regex!(any_digit_multi, r"\d", "abc123", true);
+    test_regex!(any_digit_multi_not, r"\d", "abc", false);
 
-    #[test]
-    fn test_sequence() {
-        let regex = Regex::from(r"\w\d".to_string());
-        assert!(regex.matches("a1"));
-        assert!(regex.matches("b2"));
-        assert!(regex.matches("c3"));
-        assert!(!regex.matches("a"));
-        assert!(!regex.matches("1"));
-        assert!(!regex.matches(" "));
-        assert!(!regex.matches("\n"));
-    }
+    test_regex!(any_word, r"\w", "a", true);
+    test_regex!(any_word_not, r"\w", "!", false);
+    test_regex!(any_word_empty, r"\w", "", false);
+    test_regex!(any_word_multi, r"\w", "abc123", true);
+    test_regex!(any_word_multi_not, r"\w", "!@#$", false);
+    test_regex!(any_word_underscore, r"\w", "_", true);
 
-    #[test]
-    fn test_choice() {
-        let regex = Regex::from(r"[abc]".to_string());
-        assert!(regex.matches("a"));
-        assert!(regex.matches("b"));
-        assert!(regex.matches("c"));
-        assert!(!regex.matches("d"));
-        assert!(!regex.matches("e"));
-        assert!(!regex.matches("f"));
-    }
+    test_regex!(sequence, r"\w\d", "a1", true);
+    test_regex!(sequence_not, r"\w\d", "a", false);
+    test_regex!(sequence_not2, r"\w\d", "1", false);
+    test_regex!(sequence_not3, r"\w\d", " ", false);
+    test_regex!(sequence_not4, r"\w\d", "\n", false);
+
+    test_regex!(choice, r"[abc]", "a", true);
+    test_regex!(choice2, r"[abc]", "b", true);
+    test_regex!(choice3, r"[abc]", "c", true);
+    test_regex!(choice4, r"[abc]", "d", false);
+    test_regex!(choice_multi, r"[abc]", "efagh", true);
+    test_regex!(choice_multi_not, r"[abc]", "defgh", false);
+
+    test_regex!(choice_negated, r"[^abc]", "d", true);
+    test_regex!(choice_negated2, r"[^abc]", "a", false);
+    test_regex!(choice_negated_long, r"[^abc]", "defaghi", true); // Any character can be not abc
+    test_regex!(choice_negated_long2, r"[^abc]", "abcabc", false);
 }
